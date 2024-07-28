78f5632cca129680ded6d710328ed8bbeb148ab5
Jovan Trajkovic <148716098+jovan-trajkovic@users.noreply.github.com>
2024-07-28 15:25:19 +0200

    Add project files.

---
+++ Feast Planner/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Feast_Planner.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
